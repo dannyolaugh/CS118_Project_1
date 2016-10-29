d8ae878f42f9e33cbc274e9d33e5798cc6890372
jstorch33 <jstorch33@gmail.com>
2016-10-29 18:05:56 +0000

    fu danny

--- helper.cpp
+++ helper.cpp
@@ -158,3 +158,30 @@ int HttpResponse::decode(string message)
 
 
 
+string getIP(string hostname, string portNum)
+{
+  struct addrinfo hints;
+  struct addrinfo* res;
+
+  // prepare hints                                                          
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_INET; // IPv4                                        
+  hints.ai_socktype = SOCK_STREAM; // TCP                                   
+
+  // get address                                                            
+  int status = 0;
+  if ((status = getaddrinfo(hostname, portNum, &hints, &res)) != 0) {
+    std::cerr << "getaddrinfo: " << gai_strerror(status) << std::endl;
+    return 2;
+  }
+
+  for(struct addrinfo* p = res; p != 0; p = p->ai_next) {
+    // convert address to IPv4 address                                      
+    struct sockaddr_in* ipv4 = (struct sockaddr_in*)p->ai_addr;
+
+    // convert the IP to a string and print it:                             
+    inet_ntop(p->ai_family, &(ipv4->sin_addr), ipstr, sizeof(ipstr));
+  }
+  free addrinfo(res); // free the linked list     
+  return ipstr;
+}
